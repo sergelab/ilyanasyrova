735397e4f07c426f29413c86a37b247591cad3ee
Sergelab <serge@brpr.ru>
2015-04-05 16:19:33 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 jQuery(function($) {
     $(document).ready(function() {
-        if (device.tablet() || device.ipad() || device.androidTablet() || device.ipod() || device.mobile() || device.iphone() || device.android() || device.fxosPhone() || device.fxosTablet() || device.fxos() || device.blackberryTablet()) {
+        if (device.ipad() || device.androidTablet() || device.ipod() || device.mobile() || device.iphone() || device.android() || device.fxosPhone() || device.fxosTablet() || device.fxos() || device.blackberryTablet()) {
             window.location = 'http://www.m.ilyanasyrova.com';
         } else {
             $('#container').toggleClass('hidden');
